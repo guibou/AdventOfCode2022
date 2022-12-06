09a9a3d364bfe00b1bb8136e29e5ed1b9c817988
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2022-12-07 00:25:40 +0100

    Day 05

---
+++ src/Day05.hs
@@ -0,0 +1,123 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE RecordWildCards #-}
+module Day05 where
+
+import Utils
+import qualified Relude.Unsafe as Unsafe
+import Relude.Extra
+import qualified Data.Text as Text
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.Vector as Vector
+import Text.Megaparsec
+
+fileContent :: _
+fileContent = parseContent $(getFile)
+
+parseContent :: Text -> _
+parseContent t = 
+  let [a, b] = Text.splitOn "\n\n" t
+  in (parseStacks a, unsafeParse (Prelude.many parseAction) b)
+
+data Action = Action 
+  {
+    quantity :: Int,
+    origin :: Int,
+    destination :: Int
+  }
+  deriving (Show)
+
+parseAction = do
+  _ <- "move "
+  quantity <- parseNumber
+  _ <- "from "
+  origin <- parseNumber
+  _ <- "to "
+  destination <- parseNumber
+  _ <- "\n"
+  pure $ Action{..}
+
+-- * Generics
+
+
+-- * FIRST problem
+day (stacks, actions) = let
+  finalStack = foldl' (flip applyAction) stacks actions
+  in map (Unsafe.head) (Vector.toList finalStack)
+
+-- * SECOND problem
+day' (stacks, actions) = let
+  finalStack = foldl' (flip applyAction') stacks actions
+  in map (Unsafe.head) (Vector.toList finalStack)
+
+-- * Tests
+ex_stacks' = [fmt|\
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+|]
+
+ex_stacks = Vector.fromList ["NZ", "DCM", "P"]
+
+ex_actions = unsafeParse (Prelude.many parseAction) [fmt|\
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+|]
+
+trim (' ':xs) = trim xs
+trim o = o
+
+parseStacks t = Vector.fromList $ map trim $ transpose $ map (map snd . filter (\(p, c) -> p) . zip (cycle [True, False, False, False]) . drop 1 . Text.unpack) (Text.lines t)
+
+ex = parseContent [fmt|\
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+|]
+
+applyAction :: Action -> Vector [Char] -> Vector [Char]
+applyAction action input = let
+  moveCrate:rest = input Vector.! (action.origin - 1)
+  output = input Vector.// [
+     (action.origin - 1, rest),
+     (action.destination - 1, moveCrate: (input Vector.! (action.destination - 1)))
+    ]
+  in if action.quantity == 1 then output
+     else applyAction (action { quantity = action.quantity - 1}) output
+
+applyAction' :: Action -> Vector [Char] -> Vector [Char]
+applyAction' action input = let
+  (movedCrates, rest) = splitAt (action.quantity) (input Vector.! (action.origin - 1))
+  output = input Vector.// [
+     (action.origin - 1, rest),
+     (action.destination - 1, movedCrates ++ (input Vector.! (action.destination - 1)))
+    ]
+  in output
+  
+
+test :: Spec
+test = do
+  describe "simple examples" $ do
+    it "of first star" $ do
+      day ex `shouldBe` "CMZ"
+    it "of second star" $ do
+      day' ex `shouldBe` "MCD"
+  describe "works" $ do
+    it "on first star" $ do
+      day fileContent `shouldBe` "GRTSWNJHH"
+    it "on second star" $ do
+      day' fileContent `shouldBe` "QLFQDBBHM"
+--
+-- start: 23:43
+--
+-- GRTRWNJTH is not the right answer
+-- bath star at 00:22
