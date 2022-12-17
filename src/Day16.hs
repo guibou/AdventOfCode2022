244bc9ac4e299c977f7013c27cb452fa744e95ff
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2022-12-17 01:52:29 +0100

    Day 16

--- src/Day16.hs
+++ src/Day16.hs
@@ -14,6 +14,7 @@ import Data.Set qualified as Set
 import Path (shortestPath)
 import Text.Megaparsec
 import Utils
+import System.IO.Unsafe (unsafePerformIO)
 
 fileContent :: _
 fileContent = parseContent $(getFile)
@@ -44,7 +45,7 @@ parseValveId = (\a b -> ValveId [a, b]) <$> anySingle <*> anySingle
 
 newtype ValveId = ValveId {unId :: String}
   deriving stock (Show, Ord, Eq)
-  deriving newtype (Hashable)
+  deriving newtype (Hashable, Memoizable)
 
 data Valve = Valve
   { name :: ValveId,
@@ -133,9 +134,10 @@ weightPath maxMinute valves path = finalAccum + (maxMinute - finalMinute) * (pre
 day :: _ -> Int
 day valves = Data.List.maximum $ map (weightPath 30 valves) $ solve valves
 
-solve' (makeASmallerGraph -> valves) = go (ValveId "AA", ValveId "AA") (Set.singleton (ValveId "AA")) (0, 0)
+solve' (makeASmallerGraph -> valves) = go (ValveId "AA", ValveId "AA") ([ValveId "AA"]) (0, 0)
   where
-    go currentValves visited currentCosts =
+    go = memoFix3 go'
+    go' recCall currentValves (Set.fromList -> visited) currentCosts =
       let motionMe = nextMotion valves visited currentValves currentCosts
           motionElephant = map swapFirst (nextMotion valves visited (swap currentValves) (swap currentCosts))
           motions = motionMe <> motionElephant
@@ -144,11 +146,8 @@ solve' (makeASmallerGraph -> valves) = go (ValveId "AA", ValveId "AA") (Set.sing
               then pure []
               else do
                 ((nextValves, nextCosts), nextVisited) <- motions
-                ((nextValves, nextCosts) :) <$> go nextValves nextVisited nextCosts
+                ((nextValves, nextCosts) :) <$> recCall nextValves (Set.toList nextVisited) nextCosts
 
-nothingSecond ((a, b), v) = ((nt a, nt b), v)
-
-nt (a, b) = (Just a, Nothing)
 swapFirst ((a, b), v) = ((swap a, swap b), v)
 
 nextMotion valves visited (currentValve, otherValve) (cost, otherCost) = do
@@ -167,7 +166,20 @@ weightPath' valves path = weightPath 26 valves (map f path) + weightPath 26 valv
 f ((a, b), (c, d)) = (a, c)
 f' ((a, b), (c, d)) = (b, d)
 
-day' valves = Data.List.maximum $ map (weightPath' valves) $ solve' valves
+cache = unsafePerformIO (newIORef 0)
+{-# NOINLINE cache #-}
+
+traceBigger :: Int -> Int
+traceBigger n = unsafePerformIO $ do
+  curN <- readIORef cache
+  if n > curN
+     then do
+       putStrLn (show n)
+       writeIORef cache n
+       pure n
+      else pure n
+
+day' valves = Data.List.maximum $ map (traceBigger . weightPath' valves) $ solve' valves
 -- * Tests
 
 ex =
@@ -196,4 +208,7 @@ test = do
     it "on first star" $ do
       day fileContent `shouldBe` 1906
     it "on second star" $ do
-      day' fileContent `shouldBe` 1238
+      day' fileContent `shouldBe` 2548
+
+-- 2142 is too low
+-- 2266 too low
