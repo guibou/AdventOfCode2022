ccdea4b5113959f150fc0c1e1cfa6cea5e2826de
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2022-12-17 15:19:29 +0100

    Use vector + modulos for rocks and actions

--- src/Day17.hs
+++ src/Day17.hs
@@ -2,10 +2,12 @@
 -- first at 12:55
 module Day17 (test) where
 
+import Data.List (findIndex)
 import Data.List qualified
 import Data.Map qualified as Map
 import Data.Set qualified as Set
 import Data.Text qualified as Text
+import Data.Vector qualified as Vector
 import Relude.Unsafe qualified
 import Utils
 
@@ -31,19 +33,18 @@ toRock = Rock . Set.fromList
 
 -- Rocks
 rocksPatterns =
-  [ -- - shape
-    toRock ((`V2` 0) <$> [0 :: Int .. 3]),
-    -- + shape
-    toRock [V2 1 0, V2 0 1, V2 1 1, V2 2 1, V2 1 2],
-    -- L shape
-    toRock [V2 0 0, V2 1 0, V2 2 0, V2 2 1, V2 2 2],
-    -- \| Shape
-    toRock $ V2 0 <$> [0 .. 3],
-    -- Square shape
-    toRock [V2 0 0, V2 0 1, V2 1 0, V2 1 1]
-  ]
-
-rocksStream = cycle rocksPatterns
+  Vector.fromList
+    [ -- - shape
+      toRock ((`V2` 0) <$> [0 :: Int .. 3]),
+      -- + shape
+      toRock [V2 1 0, V2 0 1, V2 1 1, V2 2 1, V2 1 2],
+      -- L shape
+      toRock [V2 0 0, V2 1 0, V2 2 0, V2 2 1, V2 2 2],
+      -- \| Shape
+      toRock $ V2 0 <$> [0 .. 3],
+      -- Square shape
+      toRock [V2 0 0, V2 0 1, V2 1 0, V2 1 1]
+    ]
 
 chamberWidth = 7 :: Int
 
@@ -56,8 +57,8 @@ toWorldSet (Rock s) p = Set.map (p +) s
 isInWorld (Rock s) =
   let min_x = Data.List.minimum (map (\(V2 x _) -> x) (Set.toList s))
       max_x = Data.List.maximum (map (\(V2 x _) -> x) (Set.toList s))
-      max_y = Data.List.minimum (map (\(V2 _ y) -> y) (Set.toList s))
-   in min_x >= 0 && max_x < chamberWidth && max_y >= 0
+      min_y = Data.List.minimum (map (\(V2 _ y) -> y) (Set.toList s))
+   in min_x >= 0 && max_x < chamberWidth && min_y >= 0
 
 -- * FIRST problem
 
@@ -70,31 +71,55 @@ moveBlock world rock rockPos dPos
     newPos = dPos rockPos
     rockSet = toWorldSet rock newPos
 
-solve nbBlocks initActions = startRockFall 0 mempty (take nbBlocks rocksStream) initActions
+solve nbBlocks initActions = startRockFall 0 mempty 0 0
   where
+    actionsV = Vector.fromList initActions
+
     startRockFall falledBlocks world _ _
-      | falledBlocks == nbBlocks = [world]
-    startRockFall falledBlocks world [] actions = startRockFall falledBlocks world rocksPatterns actions
-    startRockFall falledBlocks world (rock : rocks) actions = addBlockToWorld world rock startingRockPos : applyActionBlock startingRockPos actions
-      where
-        applyActionBlock rockPos [] = applyActionBlock rockPos initActions
-        applyActionBlock rockPos (a : as) = case moveBlock world rock rockPos (applyActionPos a) of
-          Just newPos -> addBlockToWorld world rock newPos : applyFallAction newPos as
-          Nothing -> applyFallAction rockPos as
+      | Just maxBlocks <- nbBlocks, maxBlocks == falledBlocks = [world]
+      -- | isCompleteFloor world && falledBlocks > 0 = [Set.fromList [V2 0 falledBlocks]]
+    startRockFall falledBlocks world rockId actionId = do
+      let rock = rocksPatterns Vector.! rockId
+      let (world', nextActionsId) = fallARock actionsV world rock actionId
+      startRockFall (falledBlocks + 1) world' ((rockId + 1) `mod` Vector.length rocksPatterns) nextActionsId
 
-        applyFallAction rockPos remainingActions = case moveBlock world rock rockPos (V2 0 (-1) +) of
-          Just newPos -> addBlockToWorld world rock newPos : applyActionBlock newPos remainingActions
-          Nothing -> startRockFall (falledBlocks + 1) (addBlockToWorld world rock rockPos) rocks remainingActions
+fallARock actionsV world rock actionId = applyActionBlock startingRockPos actionId
+  where
+    applyActionBlock rockPos actionId =
+      let a = actionsV Vector.! actionId
+          as = (actionId + 1) `mod` Vector.length actionsV
+       in case moveBlock world rock rockPos (applyActionPos a) of
+            Just newPos -> applyFallAction newPos as
+            Nothing -> applyFallAction rockPos as
 
-        max_y = case Set.toList world of
-          [] -> -1
-          l -> Data.List.maximum $ map (\(V2 _ y) -> y) l
-        startingRockPos = V2 2 (max_y + 4)
+    applyFallAction rockPos remainingActions = case moveBlock world rock rockPos (V2 0 (-1) +) of
+      Just newPos -> applyActionBlock newPos remainingActions
+      Nothing -> (addBlockToWorld world rock rockPos, remainingActions)
+
+    max_y = case Set.toList world of
+      [] -> -1
+      l -> Data.List.maximum $ map (\(V2 _ y) -> y) l
+
+    startingRockPos = V2 2 (max_y + 4)
 
 display s = Text.unpack $ str2DGrid $ Map.fromList $ map ((,"#") . (\(V2 x y) -> V2 x (-y))) (Set.toList s) <> [(V2 0 1, "+"), (V2 7 1, "+")]
 
+isCompleteFloor s = do
+  let max_y = Data.List.maximum (map (\(V2 _ y) -> y) (Set.toList s))
+  let at_y = Set.filter (\(V2 _ y) -> y == max_y) s
+  Set.size at_y == 7
+
+findFirstCompleteFloor n actions l = findIndex f (Set.empty : l)
+  where
+    lActions = length actions
+    lCubes = length rocksPatterns
+    f s = do
+      let max_y = Data.List.maximum (map (\(V2 _ y) -> y) (Set.toList s))
+      let at_y = Set.filter (\(V2 _ y) -> y == max_y) s
+      Set.size at_y >= (7 - n)
+
 day :: _ -> Int
-day actions = Data.List.maximum ((\(V2 _ y) -> y) <$> Set.toList (Relude.Unsafe.last (solve 2022 actions))) + 1
+day actions = Data.List.maximum ((\(V2 _ y) -> y) <$> Set.toList (Relude.Unsafe.last (solve (Just 2022) actions))) + 1
 
 -- * SECOND problem
 
