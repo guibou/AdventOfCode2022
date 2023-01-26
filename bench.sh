9de94074d833f7acd8c25c26bfa11e7b80efeba5
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2023-01-26 13:14:48 +0100

    bench: add another file where results are sorted by name

--- bench.sh
+++ bench.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cabal build alltests
 python bench.py | sort -k 3 -n > bench.txt
+cat bench.txt | sort -k 1 -n -> bench_by_names.txt
 gnuplot bench.gnuplot
 
 echo "Total Time:"
